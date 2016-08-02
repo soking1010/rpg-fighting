0474e667fcb6b6c9d164c44faf94528241e62483
soking1010 <charm.soking@gmail.com>
2016-08-02 16:08:17 +0800

    數據模組測試
    增加函數 d20Bonus() 調整值判斷
    增加了 weapon 欄位

--- rpgFight.js
+++ rpgFight.js
@@ -6,7 +6,8 @@ var mud = function (){
     hp: 0,
     atk: 0,
     agi:0,
-    life:true
+    life:true,
+    weapon:''
   };
 };
 
@@ -14,18 +15,19 @@ var mud = function (){
 var lion = new mud();
 lion.name = '閃電獅王';
 lion.hp = 20;
-lion.atk = 5;
-lion.agi = 5;
+lion.atk = 8;
+lion.agi = 16;
 lion.life = true;
-
+lion.weapon = '閃電獅爪';
 
 // 新增 orc
 var orc = new mud();
 orc.name = '半獸人';
 orc.hp = 20;
-orc.atk = 3;
-orc.agi = 5;
+orc.atk = 10;
+orc.agi = 12;
 orc.life = true;
+orc.weapon = '釘頭錘';
 
 // life
 var life = function(mudA){
@@ -33,9 +35,39 @@ var life = function(mudA){
     mudA.hp = mudA.hp - 5 ;
     document.write(mudA.name+"還活著"+"但牠遭受了攻擊！HP剩下"+mudA.hp+"點!<br>");
     return mudA.hp;
-};
+    };
 };
 
 life(orc)
+
+// D20 調整值判斷
+
+var d20Bonus = function(num){
+if (num >= 10){
+  // 大於10，超過20的調整值視為0
+  switch(x = num - 10){
+    case 0:x= 0;break;
+    case 1:x= 0;break;
+    case 2:x= 1;break;
+    case 3:x= 1;break;
+    case 4:x= 2;break;
+    case 5:x= 2;break;
+    case 6:x= 3;break;
+    case 7:x= 3;break;
+    case 8:x= 4;break;
+    case 9:x= 4;break;
+    case 10:x= 5;break;
+    default:x = 0;break;
+  };
+}else {
+  //小於10的調整值視為零
+  document.write("小於10<br>");
+    x = 0;
+};  
+// 輸出d20調整值
+return x
+};
+
+document.write("你的d20調整值是："+d20Bonus(10)+"<br>");
 
 
